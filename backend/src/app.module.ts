408cb6e2450a85ed532ec1d169290a2f922169cf
yassersenzap <yassersidane23@gmail.com>
2026-05-20 14:28:07 +0100

    chore(backend): add prisma service integration (#3)

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HealthModule } from './health/health.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, HealthModule],
   controllers: [AppController],
   providers: [AppService],
 })
